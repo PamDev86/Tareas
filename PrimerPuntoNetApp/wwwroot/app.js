71860893ef33b5eae252932f931df3474bdf8abb
PamDev86 <135281668+PamDev86@users.noreply.github.com>
2026-06-11 00:59:44 -0600

    wwwroot menu improvement

--- PrimerPuntoNetApp/wwwroot/app.js
+++ PrimerPuntoNetApp/wwwroot/app.js
@@ -1,39 +1,24 @@
-﻿function mostrarApp() {
-    document.getElementById("app").classList.add("active");
-}
-function ocultarApp() {
-    document.getElementById("app").classList.remove("active");
+﻿const API = "https://localhost:44351/api/tareas";
+
+// ABRIR / CERRAR MENU
+function toggleMenu() {
+    document.getElementById("sidebar").classList.toggle("active");
 }
 
-async function cargarTareas() {
-    const res = await fetch("https://localhost:44351/api/tareas");
-    const data = await res.json();
+// CAMBIAR SECCIONES
+function mostrarSeccion(id) {
 
-    const lista = document.getElementById("lista");
-    lista.innerHTML = "";
+    // Mostrar contenido general
+    document.querySelector(".contenido").classList.add("active");
 
-    data.forEach(t => {
-        /*input type="checkbox" → crea el checklist
-          checked → si está completada
-          onchange → cuando lo clickeas
-          line-through → texto tachado, solo es algo estetico*/
-        lista.innerHTML += `
-            <li class="list-group-item d-flex justify-content-left align-items-center">
-            <div>
-                <input type="checkbox"
-                    ${t.completada ? "checked" : ""}
-                    onchange="toggleCompletada(${t.id}, this.checked)">
-                <span style="${t.completada ? 'text-decoration: line-through; ' : ''}">
-                    ${t.titulo}&nbsp;&nbsp;&nbsp   
-                </span >
-            </div>
-            <button class="btn btn-danger btn-sm"
-                onclick="eliminarTarea(${t.id})">
-                Eliminar
-            </button>
-        </li>`;
-    });
+    // Ocultar todas las secciones
+    document.querySelectorAll(".seccion").forEach(s => s.classList.add("d-none"))
+
+    // Mostrar seleccionada
+    document.getElementById(id).classList.remove("d-none");
 }
+
+// CREAR TAREA
 
 async function crearTarea() {
     const titulo = document.getElementById("titulo").value;
@@ -43,7 +28,7 @@ async function crearTarea() {
         return;
     }
 
-    await fetch("https://localhost:44351/api/tareas", {
+    await fetch(API, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -55,23 +40,48 @@ async function crearTarea() {
     document.getElementById("titulo").value = "";
 
     cargarTareas(); //Refresca la lista
+
+    alert("Tarea creada");
 }
 
-async function eliminarTarea(id) {
-    await fetch(`https://localhost:44351/api/tareas/${id}`, {
-        method: "DELETE",        
-    });
+// CARGAR TAREAS
+async function cargarTareas() {
+    const res = await fetch(API);
+    const data = await res.json();
 
-    cargarTareas(); //Refresca la lista
+    const lista = document.getElementById("listaTareas");
+
+    lista.innerHTML = "";
+
+    data.forEach(t => {
+        lista.innerHTML += `
+            <li class="list-group-item d-flex justify-content-left align-items-center">
+
+                <div>
+                    <input type="checkbox"
+                        ${t.completada ? "checked" : ""}
+                        onchange="toggleCompletada(${t.id}, this.checked)">
+                        <span style="${t.completada ? 'text-decoration: line-through; ' : ''}">
+                            ${t.titulo}&nbsp;&nbsp;&nbsp   
+                        </span >
+                </div>
+
+                <button class="btn btn-danger btn-sm" onclick="eliminarTarea(${t.id})">
+                    Eliminar
+                </button>
+            
+            </li>
+        `;
+    });
 }
 
 async function toggleCompletada(id, estado) {
-    const res = await fetch(`https://localhost:44351/api/tareas`);
+    const res = await fetch(API);
     const tareas = await res.json();
 
     const tarea = tareas.find(t => t.id === id);
 
-    await fetch(`https://localhost:44351/api/tareas/${id}`, {
+    await fetch(`${API}/${id}`, {
         method: "PUT",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({
@@ -85,4 +95,64 @@ async function toggleCompletada(id, estado) {
     cargarTareas(); //Refresca la lista
 }
 
+// ELIMINAR TAREA
+async function eliminarTarea(id) {
+    await fetch(`${API}/${id}`, {
+        method: "DELETE",
+    });
+
+    cargarTareas(); //Refresca la lista
+}
+
+// PDF
+async function generarPDF() {
+    const res = await fetch(API);
+    const tareas = await res.json();
+
+    const { jsPDF } = window.jspdf;
+
+    const doc = new jsPDF();
+
+    //Titulo
+    doc.setFontSize(18);
+    doc.text("Lista de tareas", 20, 20);
+
+    //Encabezados
+    let y = 40;
+
+    doc.setFillColor(46, 204, 113); //Verde
+
+    doc.setFontSize(12);
+    doc.rect(20, y - 7, 120, 10, "FD");//Columna Tarea: horizonal, vertical, ancho del rectangulo, alto rectangulo, estilo
+    doc.rect(140, y - 7, 40, 10, "FD");//Columna Completada: x, y, ancho, alto, estilo
+
+    doc.setFont(undefined, "bold");
+    doc.text("Tarea", 25, y);
+    doc.text("Completada", 145, y);
+
+    doc.setFont(undefined, "normal");
+
+    y += 10;
+
+    //Filas
+    tareas.forEach((t, index) => {
+
+        doc.rect(20, y - 7, 120, 10);
+        doc.rect(140, y - 7, 40, 10);
+
+        doc.text(t.titulo, 25, y);
+
+        if (t.completada)
+            doc.text("X", 155, y);
+
+        y += 10;
+    });
+
+    doc.setFontSize(10);
+    doc.text(`Generado: ${new Date().toLocaleDateString()}`, 20, 30);
+
+    doc.save("tareas.pdf");
+}
+
+// INICIO
 cargarTareas();
